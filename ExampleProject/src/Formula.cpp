7c0b169b487d1a42c146b8468299e91d0e40055e
mamta <mamta.devi@mobeewave.com>
2019-12-02 15:13:20 -0500

    Running with a static library

---
+++ ExampleProject/src/Formula.cpp
@@ -0,0 +1,9 @@
+#include "Formula.h"
+
+int  Formula::bla(int arg1) {
+    return arg1 * 2;
+}
+
+
+
+
---
+++ ExampleProject/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Formula.h"
+
+int main() {
+    std::cout << "Bla: " << Formula::bla(2) << std::endl;
+    return 0;
+}
+
+
+
